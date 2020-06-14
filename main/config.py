5adfaee5cd13cbf8486bd9728e84ad990b7c0b87
mumin630 <mumin.light@gmail.com>
2020-06-14 09:54:50 -0700

    ベースの作成

--- main/config.py
+++
@@ -1,5 +0,0 @@
-import os
-
-SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or "sqlite:///test.db"
-SQLALCHEMY_TRACK_MODIFICATIONS = True
-SECRET_KEY="SAKURAMIONA"
--- main/views.py
+++
@@ -1,8 +0,0 @@
-import flask 
-from main import app
-from main.models import Entry
-
-@app.route('/')
-def show_entries():
-    entries = Entry.query.all()
-    return flask.render_template('entries.html', entries=entries)
